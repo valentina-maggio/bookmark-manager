313e861970976c78f498913e748b2b67e3ac59c0
Valentina <maggio.vale@gmail.com>
2022-03-16 10:48:12 +0000

    Update feature test

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -5,9 +5,9 @@ feature 'can view bookmarks' do
 
     connection = PG.connect(dbname: 'bookmark_manager_test')
       
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
 
     visit '/bookmarks/index'
 
